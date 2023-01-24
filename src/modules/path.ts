e179419df9488afb301a094e348c62237479073f
AbdulazizAssainar <abdlaziz.a.abdullah@gmail.com>
2023-01-24 14:25:57 +0200

    commit

---
+++ src/modules/path.ts
@@ -0,0 +1,4 @@
+import path from 'path'
+
+export const srcPath = path.join(__dirname, "../src");
+export const pagePath = path.join(__dirname, "../pages");
---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';   
+import { getDeviceType } from '../modules/deviceType';
+import { pagePath } from '../modules/path';
+const routes = express.Router();
+
+routes.get('/', async (req, res) => { 
+    const dtype: string = await getDeviceType(req)
+    if (dtype == "mobile") {console.log(dtype); return res.sendFile(pagePath + "/notsup.html")}
+    if (dtype == "tablet") {console.log(dtype); return res.sendFile(pagePath + "/notsup.html")}
+    if (dtype == "desktop") {console.log(dtype); return res.sendFile(pagePath + "/notsup.html")}
+    res.send('unknowen device');
+ });
+
+export default routes;
