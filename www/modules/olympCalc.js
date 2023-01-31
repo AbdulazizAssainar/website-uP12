04f0f31ae69e7b550c34432b689f281f253703b4
AbdulazizAssainar <abdlaziz.a.abdullah@gmail.com>
2023-01-31 14:33:16 +0200

    'commit'

--- www/modules/olympCalc.js
+++ www/modules/olympCalc.js
@@ -5,17 +5,9 @@ function calcData() {
     const getDays = document.getElementById("days").value;
     let Net;
     let Balance = getCapital;
-    let array;
     for (var i = 0; i < getDays; i++) {
         Balance = Number(Balance) + Number(Balance * getRatio);
         Net = Balance - getCapital;
-        array.push({
-            net: String(Math.trunc(Net)),
-            balace: String(Math.trunc(Balance)),
-            capital: String(getCapital),
-            ratio: String(getRatio),
-            days: String(getDays)
-        });
     }
     Balance = Math.trunc(Balance);
     Net = Balance - getCapital;
